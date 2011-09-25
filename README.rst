f2546719e7d66574c768eb7f57c1be65a0c87b08
Gabriel L. Oliveira <ciberglo@gmail.com>
2011-09-25 16:42:02 -0300

    initial import

---
+++ README.rst
@@ -0,0 +1,19 @@
+Sistema Hoteleiro
+==============
+
+Trabalho para matéria de Sistemas Especialistas, utilizando uma analogia a frames.
+
+Uso
+-------------
+
+::
+
+    git clone git@github.com:gabriellima/sistema-hoteleiro.git
+    bundle install
+    rails server
+
+Observações
+-------------
+
+É mais para ter algo pronto do que para ter um produto, então sem preocupações com BDD.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistemaHoteleiro::Application.config.secret_token = 'b7a31b8cc0d2f8855e04cf028a1540529f06ce49dd5699e773ab0806a22d2ba8bd886e1c47d2aa2b7ec4968804341142771bc2787f1fb7b9df64125fad82b698'
